8e42e6437d6f5f552552aec3111ff6cbd860c8c9
Your Name <you@example.com>
2023-10-23 11:14:21 +0200

    update2

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 const plateau = document.querySelector(".container");
 let selectedPiece = null;
-
 // création des cases du plateau
 for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
@@ -12,28 +11,27 @@ for (let i = 0; i < 10; i++) {
         cases.classList.add((i + j) % 2 == 0 ? "marron" : "marronClair")
         // création des pionsNoir
         if ((i + j) % 2 === 0 && i < 4) {
-            const pion = document.createElement("div")
-            pion.classList.add("pionNoir")
-            cases.appendChild(pion)
+            const pionNoir = document.createElement("div")
+            pionNoir.classList.add("pionNoir")
+            cases.appendChild(pionNoir)
         }
         // création des pionsBlancs
         else if ((i + j) % 2 === 0 && i > 5) {
-            const pion = document.createElement("div")
-            pion.classList.add("pionBlanc")
-            cases.appendChild(pion)
+            const pionBlanc = document.createElement("div")
+            pionBlanc.classList.add("pionBlanc")
+            cases.appendChild(pionBlanc)
         }
-
         // Ajout de l'événement focus pour sélectionner le pion
         cases.addEventListener('focus', function () {
-            const pion = this.querySelector('.pionNoir, .pionBlanc');
-            if (pion) {
-                selectedPiece = pion;
+            const pionSelectionner = this.querySelector('.pionNoir, .pionBlanc');
+            if (pionSelectionner) {
+                selectedPiece = pionSelectionner;
                 console.log('Pion sélectionné :', selectedPiece);
             }
         });
         // déplacement des pions
-        cases.addEventListener('click', function () {
-            if (this.tagName === "BUTTON" && selectedPiece) {
+        cases.addEventListener('focus', function () {
+            if (selectedPiece) {
                 this.appendChild(selectedPiece);
             }
         });
