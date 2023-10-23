d817d2890ad04d74e6fe1fc791f1fa6b17cd69c1
Your Name <you@example.com>
2023-10-23 11:13:34 +0200

    update2

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-"use strict"
-document.addEventListener("DOMContentLoaded", function () {
-    const plateau = document.querySelector(".container");
-    const size = 10;
+"use strict";
 
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            const cases = document.createElement("div");
-            cases.classList.add((i + j) % 2 === 0 ? "marron" : "marronClair");
+const plateau = document.querySelector(".container");
+let selectedPiece = null;
 
-            if ((i + j) % 2 === 0 && i < 4) {
-                const pieceNoire = document.createElement("div");
-                pieceNoire.classList.add("pionNoir");
-                cases.appendChild(pieceNoire);
-            }
-
-            if ((i + j) % 2 === 0 && i > 5) {
-                const pieceBlanche = document.createElement("div");
-                pieceBlanche.classList.add("pionBlanc");
-                cases.appendChild(pieceBlanche);
-            }
-
-            plateau.appendChild(cases);
+// création des cases du plateau
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        // création des cases
+        const cases = document.createElement(((i + j) % 2 == 0 ? "button" : "div"))
+        plateau.appendChild(cases)
+        cases.classList.add((i + j) % 2 == 0 ? "marron" : "marronClair")
+        // création des pionsNoir
+        if ((i + j) % 2 === 0 && i < 4) {
+            const pion = document.createElement("div")
+            pion.classList.add("pionNoir")
+            cases.appendChild(pion)
         }
+        // création des pionsBlancs
+        else if ((i + j) % 2 === 0 && i > 5) {
+            const pion = document.createElement("div")
+            pion.classList.add("pionBlanc")
+            cases.appendChild(pion)
+        }
+
+        // Ajout de l'événement focus pour sélectionner le pion
+        cases.addEventListener('focus', function () {
+            const pion = this.querySelector('.pionNoir, .pionBlanc');
+            if (pion) {
+                selectedPiece = pion;
+                console.log('Pion sélectionné :', selectedPiece);
+            }
+        });
+        // déplacement des pions
+        cases.addEventListener('click', function () {
+            if (this.tagName === "BUTTON" && selectedPiece) {
+                this.appendChild(selectedPiece);
+            }
+        });
     }
-});
+}
